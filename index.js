637ecabeaee87d2325997c9a4f2932eed5d9b45e
Viktor <code@viktorchin.nl>
2022-04-12 09:34:49 +0200

    Make it functional, add positioning options

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 import createEl from "@codebundlesbyvik/element-operations";
 
-class DevLabel {
-    init() {
-        if (process.env.NODE_ENV !== "development") {
-            return;
-        }
-
-        const labelEl = document.querySelector("#html-dev-label");
-
-        if (labelEl) {
-            throw new Error("html-dev-label: Exiting - instance has already been initialized!");
-        }
-
-        this.createLabel();
+export default function htmlDevLabel(position) {
+    if (process.env.NODE_ENV !== "development") {
+        return;
     }
 
-    createLabel() {
-        const elTag = "div";
-        const elId = "html-dev-label";
-        const elCss = `
-            position: fixed; bottom: 0.25rem; right: 0.25rem; z-index: 10000;
-            padding: 1rem;
-            text-transform: uppercase; font: 700 1.25rem system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif;
-            background-color: white; border: 0.25rem solid red;
-        `;
-        const elText = "Build: dev";
+    let el = document.querySelector("#html-dev-label");
 
-        const el = createEl(elTag, { id: elId });
-        el.style.cssText = elCss;
-        el.textContent = elText;
+    if (el) {
+        console.warn("html-dev-label: Exiting - label already active.");
 
-        document.body.insertBefore(el, document.body.firstElementChild);
+        return;
     }
+
+    el = createEl("div", { id: "html-dev-label" });
+
+    el.textContent = "Build: dev";
+
+    const positionArray = position ? position.toLowerCase().split(" ") : "bottom right";
+    const positionY = positionArray[0] === "top" ? "top: 0.25rem;" : "bottom: 0.25rem;";
+    const positionX = positionArray[1] === "left" ? "left: 0.25rem;" : "right: 0.25rem;";
+
+    const elCss = `
+        position: fixed; ${positionY} ${positionX} z-index: 10000;
+        padding: 1rem;
+        text-transform: uppercase; font: 700 1.25rem system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif;
+        background-color: white; border: 0.25rem solid red;
+    `;
+    el.style.cssText = elCss;
+
+    document.body.insertBefore(el, document.body.firstElementChild);
 }
-
-export default DevLabel;
