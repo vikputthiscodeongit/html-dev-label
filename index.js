af7d8c1f7ad906694321dc1305bc456aaf6c4e53
Viktor <code@viktorchin.nl>
2022-05-13 22:50:03 +0200

    Improve variable assignments

--- index.js
+++ index.js
@@ -8,26 +8,28 @@ export default function htmlDevLabel(position) {
     let el = document.querySelector("#html-dev-label");
 
     if (el) {
-        console.warn("html-dev-label: Exiting - label already active.");
+        console.warn("html-dev-label: Exiting - label is already present on page.");
 
         return;
     }
 
-    el = createEl("div", { id: "html-dev-label" });
-
-    el.textContent = "Build: dev";
-
     const positionArray = position ? position.toLowerCase().split(" ") : "bottom right";
-    const positionY = positionArray[0] === "top" ? "top: 0.25rem;" : "bottom: 0.25rem;";
-    const positionX = positionArray[1] === "left" ? "left: 0.25rem;" : "right: 0.25rem;";
+    positionArray[0] ||= "top";
+    positionArray[1] ||= "left";
 
     const elCss = `
-        position: fixed; ${positionY} ${positionX} z-index: 10000;
+        position: fixed; ${positionArray[0]}: 0.25rem; ${positionArray[1]}: 0.25rem; z-index: 10000;
         padding: 1rem;
-        text-transform: uppercase; font: 700 1.25rem system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif;
+        text-transform: uppercase; font: 700 1.25rem/1.2 system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif;
         background-color: white; border: 0.25rem solid red;
     `;
-    el.style.cssText = elCss;
+
+    el = createEl("div", {
+        id: "html-dev-label",
+        style: elCss
+    });
+
+    el.textContent = "Build: dev";
 
     document.body.insertBefore(el, document.body.firstElementChild);
 }
