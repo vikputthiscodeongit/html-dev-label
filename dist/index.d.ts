e015db88eac5a27ae1347d537cade017b40247c7
Viktor <code@viktorchin.nl>
2024-09-14 15:30:31 +0200

    build: Update distributables

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,11 @@
-type PositionY = "top" | "bottom";
-type PositionX = "left" | "right";
-interface DefaultOptions {
-    parentEl: HTMLElement;
-    position: [PositionY, PositionX];
-    size: "sm" | "md" | "lg";
-    text: string;
-    fontFamily: "monospace" | "sans-serif" | "inherit";
-    backgroundColor: string;
-    borderColor: string;
+interface Options {
+    parentEl?: HTMLElement;
+    position?: ["top" | "bottom", "left" | "right"];
+    size?: "sm" | "md" | "lg";
+    text?: string;
+    fontFamily?: "monospace" | "sans-serif" | "inherit";
+    backgroundColor?: string;
+    borderColor?: string;
 }
-interface Options extends Partial<DefaultOptions> {
-}
-declare function htmlDevLabel(options?: string | [PositionY, PositionX] | Options): void;
+declare function htmlDevLabel(positionOrOptions?: Options["position"] | Options): void;
 export { htmlDevLabel as default };
